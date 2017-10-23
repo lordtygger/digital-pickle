2418891b1a33295bac04afe09e8f9464471754b2
lordtygger <thomas.thetford@tygger.com>
2017-10-23 10:01:15 -0500

    Adding initial index.php file.

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include "includes/test.php"
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Test Pages</title>
+</head>
+<body>
+
+<?php echo $character_name; ?>
+
+</body>
+</html>
